84170a7c6189411b6d9e442a8ba2f7d54423d656
Liam Kreiss <liam.kreiss@gmail.com>
2016-01-22 10:04:53 -0800

    completed 2 and 3

--- Test/src/eulerProblems/Number01.java
+++ Test/src/eulerProblems/Number01.java
@@ -2,14 +2,22 @@ package eulerProblems;
 
 public class Number01 {
 	/*
-	 * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-
-		Find the sum of all the multiples of 3 or 5 below 1000.
+	 * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 
+	 * 3, 5, 6 and 9. The sum of these multiples is 23.
+	 * Find the sum of all the multiples of 3 or 5 below 1000.
 	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int counter = 0;
+		for (int i = 0; i < 1000; i+=3){
+			counter += i;
+		}
+		for (int i = 0; i < 1000; i+=5){
+			if (i % 3 != 0){
+				counter += i;
+			}
+		}
+		System.out.println(counter);
 	}
 
 }
