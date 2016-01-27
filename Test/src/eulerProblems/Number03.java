c0271ea06860bbfadc3e28c833c7af2f037f8317
Liam Kreiss <liam.kreiss@gmail.com>
2016-01-27 07:44:41 -0800

    changed name of #5 to explore a different solution method

--- Test/src/eulerProblems/Number03.java
+++ Test/src/eulerProblems/Number03.java
@@ -1,16 +1,16 @@
 package eulerProblems;
 
-import java.util.ArrayList;
-
 public class Number03 {
 	
 	/*
 	 * The prime factors of 13195 are 5, 7, 13 and 29.
 	 * What is the largest prime factor of the number 600851475143 ?
 	 */
+	
+	public static final long NUM = 600851475143L;
 
 	public static void main(String[] args) {
-		System.out.println(primeFactorize(600851475143L));
+		System.out.println(primeFactorize(NUM));
 
 	}
 	
---
+++ Test/src/eulerProblems/Number05Attempt2.java
@@ -0,0 +1,73 @@
+package eulerProblems;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Number05Attempt2 {
+
+	/*
+	 * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 
+	 * without any remainder.
+	 * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+	 */
+
+	public static void main(String[] args) {
+		ArrayList<Integer> allFactors = new ArrayList<Integer>();
+		for (int i = 2; i <= 20; i++){
+			ArrayList<Integer> factors = primeFactorize(i);
+			update(allFactors, factors);
+			Collections.sort(allFactors);
+		}
+		System.out.println(allFactors);
+	}
+	
+	private static void update(ArrayList<Integer> allFactors, ArrayList<Integer> factors) {
+		int nextVal = 0;
+		for (int i : factors){
+			if (allFactors.size() > nextVal){
+				if (allFactors.get(nextVal) == i){
+				} else {
+					allFactors.add(i);
+				}
+			} else {
+				allFactors.add(i);
+			}
+			nextVal++;
+		}
+	}
+
+	private static ArrayList<Integer> primeFactorize(int i) {
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		
+		if (i == 1){
+			primes.add(1);
+			return primes;
+		}
+		
+		boolean done = false;
+		while (i % 2 == 0){
+			primes.add(2);
+			if (i == 2){
+				done = true;
+			}
+			i /= 2;
+		}
+		
+		int j = 3;
+		while (!done){
+			if (i % j == 0){
+				primes.add(j);
+				if (i == j){
+					done = true;
+				}
+				i /= j;
+			}
+			if (i % j != 0){
+				j += 2;
+			}
+		}
+		
+		return primes;
+	}
+
+}
